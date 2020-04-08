dd85cf35db6a4c3434c475752be4e53de676e4c0
StreetDream <mishkasaltovskiy@gmail.com>
2020-04-08 09:13:49 +0300

    added section product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,18 @@ $(function(){
     var mixer = mixitup('.products__inner-box');
 
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+      $('.product-slider__inner').slick({
+          dots: true,
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          arrows: false
+      });
+
 
 });
