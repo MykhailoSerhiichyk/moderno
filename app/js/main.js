1d35e4a48255b69bb2adb58e3a1d15e74905067b
StreetDream <mishkasaltovskiy@gmail.com>
2020-04-12 13:05:34 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,7 @@ $(function(){
 
     var mixer = mixitup('.products__inner-box');
 
-
-    $(".rate-star").rateYo({
-        rating: 5,
-        starWidth: "12px",
-        readOnly: true
-      });
+    
 
       $('.product-slider__inner').slick({
           dots: true,
@@ -16,5 +11,37 @@ $(function(){
           arrows: false
       });
 
+      
 
+      
+      
+
+
+});
+
+$('.js-range-slider').ionRangeSlider({
+  type: "double",
+  min: 0,
+  max: 1000,
+  from: 0,
+  to: 600,
+  prefix: "$"
+});
+
+$(".rate-star").rateYo({
+rating: 5,
+starWidth: "12px",
+readOnly: true
+});
+
+$('.icon-th-list').on('click', function(){
+  $('.product__item').addClass('list');
+  $('.icon-th-list').addClass('active');
+  $('.icon-th-large').removeClass('active')
+});
+
+$('.icon-th-large').on('click', function(){
+  $('.product__item').removeClass('list');
+  $('.icon-th-large').addClass('active');
+  $('.icon-th-list').removeClass('active')
 });
